4b5dcfc1baf3bf36fa93a890d876f41a67437ec2
JavierPetracca32 <javier.petracca@gmail.com>
2023-05-21 17:04:44 -0300

    TP10

---
+++ index.js
@@ -0,0 +1,14 @@
+let Nombre="Javier"
+let Apellido="Petracca"
+console.log(Nombre+" "+ Apellido)
+
+let Edad=37
+let Nacimiento=1986
+let AnoActual=(Edad+Nacimiento)
+console.log(AnoActual)
+
+let Dias365=1
+let AnoProximo=(AnoActual+Dias365)
+console.log(AnoProximo)
+
+
